e02b485076bbe196e80bbdd60065df46a1c6c968
vinhnghiep0811 <vinhnghieple0811@gmail.com>
2024-11-27 12:40:48 +0700

    add/delete printers

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const userRouter = require('./userRouter');
 const authRoter = require('./authRouter');
+const adminRouter = require('./adminRouter');
 
 function route(app) {
   app.use('/login', authRoter);
   app.use('/user', userRouter);
+  app.use('/admin', adminRouter);
 }
 
 module.exports = route;
